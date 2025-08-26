7fb573b9bee55df6649141330fea9f0efd65041e
shammah <shammahsemali@gmail.com>
2025-08-26 14:45:19 -0400

    Fix task_4.sql with correct TABLE_NAME = 'Books'

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
 -- Switch to the correct database
 USE alx_book_store;
 
--- Get full description of the books table using INFORMATION_SCHEMA
+-- Get full description of the Books table using INFORMATION_SCHEMA
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
+  AND TABLE_NAME = 'Books';
